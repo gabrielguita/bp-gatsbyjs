d42b27bd7f2a5ff835598fede602588a053a1bbf
Gabriel Guita <ionut.guita@gmail.com>
2018-12-20 14:39:49 +0100

    cleaning code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import { render } from "react-dom";
-// importing from "react-emotion" for consistency.
 import styled from "@emotion/styled";
 import { css } from "@emotion/core";
 
